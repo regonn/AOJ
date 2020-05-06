04ada5b04869b5f21a6942f749ac35259e6fc481
regonn <programmer@haizine.art>
2020-05-06 15:41:38 +0900

    wip ALDS1_8_A

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,56 @@ struct Node {
 }
 
 struct Tree {
+    root: Option<NodeId>,
     nodes: Vec<Node>,
+}
+
+impl Tree {
+    fn new() -> Tree {
+        Tree {
+            root: None,
+            nodes: Vec::new(),
+        }
+    }
+
+    fn insert(&mut self, value: i64) {
+        let mut y: Option<NodeId> = None;
+        let mut x: Option<NodeId> = self.root;
+
+        while x != None {}
+
+        let mut node = Node {
+            parent: y,
+            value: value,
+            left: None,
+            right: None,
+        };
+    }
+
+    fn print(&mut self) {
+        inorder(0, &self.nodes);
+        preorder(0, &self.nodes);
+    }
+}
+
+fn preorder(node_id: NodeId, nodes: &Vec<Node>) {
+    print!(" {}", nodes[node_id].value);
+    if nodes[node_id].left != None {
+        preorder(nodes[node_id].left.unwrap(), nodes)
+    }
+    if nodes[node_id].right != None {
+        preorder(nodes[node_id].right.unwrap(), nodes)
+    }
+}
+
+fn inorder(node_id: NodeId, nodes: &Vec<Node>) {
+    if nodes[node_id].left != None {
+        inorder(nodes[node_id].left.unwrap(), nodes)
+    }
+    print!(" {}", nodes[node_id].value);
+    if nodes[node_id].right != None {
+        inorder(nodes[node_id].right.unwrap(), nodes)
+    }
 }
 
 fn read<T: FromStr>() -> T {
@@ -28,6 +77,14 @@ fn read<T: FromStr>() -> T {
 
 fn main() {
     let n: usize = read();
-
-    let mut tree: Tree = vec![];
+    let mut tree: Tree = Tree::new();
+    for _ in 0..n {
+        let command: String = read();
+        if command == "insert" {
+            let number: i64 = read();
+            tree.insert(number);
+        } else if command == "print" {
+            tree.print();
+        }
+    }
 }
