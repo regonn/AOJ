96822f64bbde23ce809824f146ab455d78d379d7
regonn <programmer@haizine.art>
2020-05-22 21:21:24 +0900

    wip ALDS1_9_A

--- ALDS1_9/wip_A.rs
+++ ALDS1_9/wip_A.rs
@@ -17,15 +17,34 @@ fn parent(node_id: usize) -> usize {
     return node_id / 2;
 }
 
+fn left(node_id: usize) -> usize {
+    return node_id * 2;
+}
+
+fn right(node_id: usize) -> usize {
+    return node_id * 2 + 1;
+}
+
 fn main() {
     let h: usize = read();
-    let a: Vec<i64> = (0..h)
+    let a: Vec<i64> = (1..h)
         .map(|_| {
             let number: i64 = read();
             return number;
         })
         .collect();
     for (index, node_key) in a.iter().enumerate() {
-        print!("node {}: key = {}", index + 1, node_key);
+        let current_node_id = index + 1;
+        print!("node {}: key = {}, ", current_node_id, node_key);
+        if parent(current_node_id) >= 1 {
+            print!("parent key = {}, ", a[parent(current_node_id)]);
+        }
+        if left(current_node_id) < h {
+            print!("left key = {}, ", a[left(current_node_id)]);
+        }
+        if right(current_node_id) < h {
+            print!("right key = {}, ", a[right(current_node_id)]);
+        }
+        println!();
     }
 }
