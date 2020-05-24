b96236da2e42a914117f5f0dd42a22607c56066f
regonn <programmer@haizine.art>
2020-05-24 18:01:37 +0900

    ALDS1_9_B

--- ALDS1_9/wip_B.rs
+++ ALDS1_9/wip_B.rs
@@ -13,9 +13,38 @@ fn read<T: FromStr>() -> T {
     token.parse().ok().expect("failed to parse token")
 }
 
-fn max_heapify(mut a: &Vec<i64>, node_id: usize) {}
+fn left(node_id: usize) -> usize {
+    return node_id * 2;
+}
 
-fn build_max_heap(mut a: &Vec<i64>) {}
+fn right(node_id: usize) -> usize {
+    return node_id * 2 + 1;
+}
+
+fn max_heapify(a: &mut Vec<i64>, node_id: usize) {
+    let left = left(node_id);
+    let right = right(node_id);
+    let mut largest: usize;
+    if left <= (a.len() - 1) && a[left] > a[right] {
+        largest = left;
+    } else {
+        largest = node_id;
+    }
+    if right <= (a.len() - 1) && a[right] > a[largest] {
+        largest = right;
+    }
+
+    if largest != node_id {
+        a.swap(node_id, largest);
+        max_heapify(a, largest);
+    }
+}
+
+fn build_max_heap(a: &mut Vec<i64>) {
+    for i in (1..(((a.len() - 1) / 2) + 1)).rev() {
+        max_heapify(a, i);
+    }
+}
 
 fn main() {
     let h: usize = read();
@@ -24,5 +53,5 @@ fn main() {
         let number: i64 = read();
         a.push(number);
     }
-    build_max_heap(&a);
+    build_max_heap(&mut a);
 }
