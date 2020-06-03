f70d1be17049cfe37be843e6d7a05650bd3126c9
regonn <programmer@haizine.art>
2020-06-03 11:07:23 +0900

    wip ALDS1_10_B

--- ALDS1_10/wip_B.rs
+++ ALDS1_10/wip_B.rs
@@ -17,24 +17,33 @@ fn read<T: FromStr>() -> T {
 fn main() {
     let n: usize = read();
     let mut p: Vec<u32> = vec![0; n + 1];
-    let mut m: Vec<Vec<u32>> = vec![vec![0; n + 1]; n + 1];
+    let mut m: Vec<Vec<u32>> = vec![vec![u32::max_value(); n + 1]; n + 1];
     for index in 0..n {
         let r: u32 = read();
         let c: u32 = read();
         if index == 0 {
-            p.push(r);
-            p.push(c);
+            p[index] = r;
+            p[index + 1] = c;
         } else {
-            p.push(c);
+            p[index + 1] = c;
         }
     }
+    for index in 1..n {
+        m[index][index] = 0;
+    }
     for index_i in 2..(n + 1) {
-        for index_j in 1..(n - index_i) {
+        for index_j in 1..(n - index_i + 2) {
             let index_k = index_i + index_j - 1;
-            m[index_j][index_k] = u32::max_value();
             for index_l in index_j..index_k {
-                m[index_j][index_k] = min(m[index_j][index_l], 0)
+                m[index_j][index_k] = min(
+                    m[index_j][index_k],
+                    m[index_j][index_l]
+                        + m[index_l + 1][index_k]
+                        + p[index_j - 1] * p[index_l] * p[index_k],
+                );
+                println!("{}", m[index_j][index_k]);
             }
         }
     }
+    println!("{}", m[1][n]);
 }
