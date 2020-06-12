dc5752ef937b70d8efaf226949ecd33308e8f34e
regonn <programmer@haizine.art>
2020-06-12 16:00:00 +0900

    wip ALDS_10_D

--- src/main.rs
+++ src/main.rs
@@ -27,4 +27,40 @@ fn main() {
             number
         })
         .collect();
+
+    let mut k: Vec<Vec<f32>> = vec![vec![0.0: n + 1]: n + 1];
+    let mut c: Vec<Vec<f32>> = vec![vec![0.0: n + 1]: n + 1];
+    let mut s: Vec<f32> = vec![0.0: ((2 * n) + 1)];
+    for index in 0..2*n+1 {
+        s[index +1] = s[i] + (p[i/2] if i % 2 else q[1/2])
+    }
+
+    for index in 0..n+1{
+        c[index][index] = q[index];
+        k[index][index] = index as f32;
+    }
+
+    for index_i in 1..n+1{
+        for index_j in 0..n+1 - index_i{
+            let m = index_i + index_j
+            let k0 = k[index_j][m -1];
+            let k1 = k[index_j+1][m];
+
+            let temp = 1e30;
+
+            let k2 = None;
+
+            for index_k in k0..min(k1+1, m){
+                let v = c[index_j][index_k] + c[index_k + 1][m];
+                if v < temp {
+                    k2 = index_k;
+                    temp = v;
+
+                }
+            }
+            k[index_j][m] = k2
+            c[index_j][m] = temp + (s[2*m+1] - s[2*index_j])
+        }
+    }
+    println!("{}", c[0][n]);
 }
