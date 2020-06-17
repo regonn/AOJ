68075d06fb67acdf8bd7e17a57d9cec522e2e6e6
regonn <programmer@haizine.art>
2020-06-17 12:22:44 +0900

    wip ALDS1_11_B

--- ALDS1_11/wip_B.rs
+++ ALDS1_11/wip_B.rs
@@ -17,11 +17,29 @@ const WHITE: u8 = 0;
 const GRAY: u8 = 1;
 const BLACK: u8 = 2;
 
+fn dfs_visit(
+    m: &Vec<Vec<u8>>,
+    index: usize,
+    color: &mut Vec<u8>,
+    time: &mut u32,
+    d: &mut Vec<u32>,
+    f: &mut Vec<u32>,
+) {
+    *time = *time + 1;
+    color[index] = GRAY;
+    d[index] = *time;
+    for index_target in 0..color.len() {
+        if m[index][index_target] == 0 {}
+    }
+}
+
 fn main() {
     let n: usize = read();
-    let mut color: Vec<u8> = vec![0; n];
-    let mut stack: Vec<usize> = vec![];
+    let mut color: Vec<u8> = vec![WHITE; n];
     let mut m: Vec<Vec<u8>> = vec![vec![0; n]; n];
+    let mut d: Vec<u32> = vec![0; n];
+    let mut f: Vec<u32> = vec![0; n];
+    let mut time: u32 = 0;
     for _ in 0..n {
         let index: usize = read();
         let count: usize = read();
@@ -29,5 +47,13 @@ fn main() {
             let number: usize = read();
             m[index - 1][number - 1] = 1;
         }
+    }
+    for index in 0..n {
+        if color[index] == WHITE {
+            dfs_visit(&m, index, &mut color, &mut time, &mut d, &mut f);
+        }
+    }
+    for index in 0..n {
+        println!("{} {} {}", index + 1, d[index], f[index]);
     }
 }
