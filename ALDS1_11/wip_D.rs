040b1e0689e87a819e68bb21db9f9318fa0e3953
regonn <programmer@haizine.art>
2020-06-27 16:14:26 +0900

    ALDS1_11_D

--- ALDS1_11/wip_D.rs
+++ ALDS1_11/wip_D.rs
@@ -14,8 +14,56 @@ fn read<T: FromStr>() -> T {
     token.parse().ok().expect("failed to parse token")
 }
 
+const INFINITY: u64 = 10000000;
+
+fn bfs(m: &Vec<Vec<bool>>, n: usize, from: usize, to: usize) -> bool {
+    let mut d: Vec<u64> = vec![INFINITY; n];
+    let mut q: VecDeque<u64> = VecDeque::new();
+    q.push_back(from as u64);
+    d[from] = 0;
+    while q.len() > 0 {
+        let u: usize = q.pop_front().unwrap() as usize;
+        for v in 0..n {
+            if !m[u][v] {
+                continue;
+            }
+            if d[v] != INFINITY {
+                continue;
+            }
+            d[v] = d[u] + 1;
+            q.push_back(v as u64);
+        }
+    }
+    
+    for index in 0..d.len() {
+        let number: i64;
+        if d[index] == INFINITY {
+            number = -1;
+        } else {
+            number = d[index] as i64;
+        }
+        println!("{} {}", index + 1, number);
+    }
+}
+
 fn main() {
     let n: usize = read();
     let m: usize = read();
-    let mut frinds: Vec<Vec<bool>> = vec![vec![false; n]; n];
+    let mut friends: Vec<Vec<bool>> = vec![vec![false; n]; n];
+    for _ in 0..m {
+        let person_index1: usize = read();
+        let person_index2: usize = read();
+        friends[person_index1][person_index2] = true;
+        friends[person_index2][person_index1] = true;
+    }
+    let question_count: usize = read();
+    for _ in 0..question_count {
+        let from: usize = read();
+        let to: usize = read();
+        if bfs(friends, n, from, to) {
+            println!("yes");
+        } else {
+            println!("no");
+        }
+    }
 }
