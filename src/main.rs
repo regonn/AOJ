419d6a44de1c24de57c85ec80f2cccd52ed00f86
regonn <programmer@haizine.art>
2020-06-28 12:28:12 +0900

    wip ALDS1_11_D

--- src/main.rs
+++ src/main.rs
@@ -16,34 +16,29 @@ fn read<T: FromStr>() -> T {
 
 const INFINITY: u64 = 10000000;
 
-fn bfs(m: &Vec<Vec<bool>>, n: usize, from: usize, to: usize) -> bool {
+fn bfs(friends: &Vec<Vec<bool>>, n: usize) -> Vec<Vec<bool>> {
     let mut d: Vec<u64> = vec![INFINITY; n];
     let mut q: VecDeque<u64> = VecDeque::new();
-    q.push_back(from as u64);
-    d[from] = 0;
-    while q.len() > 0 {
-        let u: usize = q.pop_front().unwrap() as usize;
-        for v in 0..n {
-            if !m[u][v] {
-                continue;
+    let mut connected_friends: Vec<Vec<bool>> = vec![vec![false; n]; n];
+    for index in 0..n {
+        q.push_back(index as u64);
+        d[index] = 0;
+        while q.len() > 0 {
+            let u: usize = q.pop_front().unwrap() as usize;
+            for v in 0..n {
+                if !friends[u][v] {
+                    continue;
+                }
+                if d[v] != INFINITY {
+                    continue;
+                }
+                d[v] = d[u] + 1;
+                q.push_back(v as u64);
             }
-            if d[v] != INFINITY {
-                continue;
-            }
-            d[v] = d[u] + 1;
-            q.push_back(v as u64);
         }
     }
-    
-    for index in 0..d.len() {
-        let number: i64;
-        if d[index] == INFINITY {
-            number = -1;
-        } else {
-            number = d[index] as i64;
-        }
-        println!("{} {}", index + 1, number);
-    }
+
+    return connected_friends;
 }
 
 fn main() {
@@ -57,10 +52,11 @@ fn main() {
         friends[person_index2][person_index1] = true;
     }
     let question_count: usize = read();
+    let connected_friends: Vec<Vec<bool>> = bfs(&friends, n);
     for _ in 0..question_count {
         let from: usize = read();
         let to: usize = read();
-        if bfs(friends, n, from, to) {
+        if connected_friends[from][to] {
             println!("yes");
         } else {
             println!("no");
