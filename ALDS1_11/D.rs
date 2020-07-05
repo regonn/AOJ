eca80a39ffaa90d4080ae62d3f93507a14f5962f
regonn <programmer@haizine.art>
2020-07-05 11:40:26 +0900

    ALDS1_11_D

--- ALDS1_11/D.rs
+++ ALDS1_11/D.rs
@@ -14,12 +14,7 @@ fn read<T: FromStr>() -> T {
     token.parse().ok().expect("failed to parse token")
 }
 
-fn dfs(
-    color: &mut Vec<Option<usize>>,
-    target: usize,
-    id: usize,
-    g: &mut Vec<Vec<Option<usize>>>,
-) {
+fn dfs(color: &mut Vec<Option<usize>>, target: usize, id: usize, g: &mut Vec<Vec<usize>>) {
     let mut s: VecDeque<usize> = VecDeque::new();
     s.push_back(target);
     color[target] = Some(id);
@@ -27,18 +22,15 @@ fn dfs(
     while s.len() > 0 {
         let u: usize = s.pop_front().unwrap() as usize;
         for i in 0..g[u].len() {
-            if g[u][i] != None {
-                let v: usize = g[u][i].unwrap();
-                if color[v] == None {
-                    color[v] = Some(id);
-                    s.push_back(v)
-                }
+            let v: usize = g[u][i];
+            if color[v] == None {
+                dfs(color, v, id, g);
             }
         }
     }
 }
 
-fn assign_color(color: &mut Vec<Option<usize>>, g: &mut Vec<Vec<Option<usize>>>, n: usize) {
+fn assign_color(color: &mut Vec<Option<usize>>, g: &mut Vec<Vec<usize>>, n: usize) {
     let mut id: usize = 1;
     for u in 0..n {
         if color[u] == None {
@@ -51,13 +43,13 @@ fn assign_color(color: &mut Vec<Option<usize>>, g: &mut Vec<Vec<Option<usize>>>,
 fn main() {
     let n: usize = read();
     let m: usize = read();
-    let mut g: Vec<Vec<Option<usize>>> = vec![vec![None; n]; n];
+    let mut g: Vec<Vec<usize>> = vec![vec![]; n];
     let mut color: Vec<Option<usize>> = vec![None; n];
     for _ in 0..m {
         let person_index1: usize = read();
         let person_index2: usize = read();
-        g[person_index1].push(Some(person_index2));
-        g[person_index2].push(Some(person_index1));
+        g[person_index1].push(person_index2);
+        g[person_index2].push(person_index1);
     }
     assign_color(&mut color, &mut g, n);
     let question_count: usize = read();
