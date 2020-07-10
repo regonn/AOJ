d70a7122b5fc93176fbb637c97bc8213cc5d2508
regonn <programmer@haizine.art>
2020-07-10 09:36:42 +0900

    ALDS1_12_C

--- src/main.rs
+++ src/main.rs
@@ -20,45 +20,37 @@ const BLACK: u32 = 2;
 const INFINITY: u32 = 1000000;
 const MAX: usize = 10000;
 
-fn dijkstra(m: &mut Vec<(usize, u32)>, n: usize) {
-    let mut pq: VecDeque<(usize, u32)> = VecDeque::new();
-    let mut d: Vec<u32> = vec![0; MAX];
-    let mut color: Vec<u32> = vec![0; MAX];
+fn dijkstra(adj: &mut Vec<Vec<(usize, u32)>>, n: usize) {
+    let mut pq: VecDeque<(i32, u32)> = VecDeque::new();
+    let mut d: Vec<u32> = vec![INFINITY; MAX];
+    let mut color: Vec<u32> = vec![WHITE; MAX];
 
     d[0] = 0;
     pq.push_back((0, 0));
     color[0] = GRAY;
 
     while pq.len() > 0 {
-        let (first, u): (usize, u32) = pq.pop_front().unwrap();
+        let (first, u): (i32, u32) = pq.pop_front().unwrap();
 
         color[u as usize] = GRAY;
 
-        if d[u as usize] as i32 < (first as i32) * (-1) {
-            continue
+        if (d[u as usize] as i32) < first * -1 {
+            continue;
         }
 
-        let mut minv = INFINITY;
-        let mut u: i32 = -1;
-        for index in 0..n {
-            if minv > d[index] && color[index] != BLACK {
-                u = index as i32;
-                minv = d[index]
+        for j in 0..adj[u as usize].len() {
+            let (v, second) = adj[u as usize][j];
+            if color[v] == BLACK {
+                continue;
             }
-        }
-        if u == -1 {
-            break;
-        }
-        color[u as usize] = BLACK;
-        for v in 0..n {
-            if color[v] != BLACK && adj[u as usize][v] != INFINITY {
-                if d[v] > d[u as usize] + m[u as usize][v] {
-                    d[v] = d[u as usize] + m[u as usize][v];
-                    color[v] = GRAY
-                }
+            if d[v] > d[u as usize] + second {
+                d[v] = d[u as usize] + second;
+                pq.push_back(((d[v] as i32 * -1), v as u32));
+                color[v as usize] = GRAY;
             }
         }
     }
+
     for index in 0..n {
         print!("{} ", index);
         println!("{}", d[index]);
@@ -67,14 +59,14 @@ fn dijkstra(m: &mut Vec<(usize, u32)>, n: usize) {
 
 fn main() {
     let n: usize = read();
-    let mut adj: Vec<(usize, u32)> = vec![(0, INFINITY); MAX];
+    let mut adj: Vec<Vec<(usize, u32)>> = vec![vec![]; MAX];
     for _ in 0..n {
         let u: usize = read();
         let k: usize = read();
         for _ in 0..k {
             let v: usize = read();
             let c: u32 = read();
-            adj[u] = (v, c);
+            adj[u].push((v, c));
         }
     }
 
