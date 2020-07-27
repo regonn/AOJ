7de509bacacb200c1f48e72cc120ea872f306683
regonn <programmer@haizine.art>
2020-07-28 08:10:23 +0900

    wip ALDS1_13_C

---
+++ ALDS1/13/wip_C.rs
@@ -0,0 +1,101 @@
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
+const N: usize = 4;
+const N2: usize = 16;
+const LIMIT: usize = 100;
+
+const DX: [i32; 4] = [-1, 0, 1, 0];
+const DY: [i32; 4] = [0, -1, 0, 1];
+const DIR: [&str; 4] = [&"u", &"l", &"d", &"r"];
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct Puzzle {
+    f: Vec<usize>,
+    space: usize,
+    md: usize,
+    cost: usize,
+}
+
+struct State {
+    puzzle: Puzzle,
+    estimated: usize,
+}
+
+fn puzzle_key(puzzle: Puzzle) -> String {
+    return puzzle.f.iter().map(|&s| s.to_string()).collect::<String>();
+}
+
+fn astar(puzzle: Puzzle) -> usize {
+    let mut q: VecDeque<Puzzle> = VecDeque::new();
+    let mut v = HashSet::new();
+    let new_puzzle = puzzle;
+    let v_key: String = puzzle_key(new_puzzle.clone());
+    q.push_back(new_puzzle);
+    v.insert(v_key);
+    while q.len() > 0 {
+        let u_puzzle: Puzzle = q.pop_front().unwrap();
+        if is_target(u_puzzle.clone()) {
+            return u_puzzle.path;
+        }
+        let sx = u_puzzle.space / N;
+        let sy = u_puzzle.space % N;
+        for r in 0..4 {
+            let tx: i32 = sx as i32 + DX[r];
+            let ty: i32 = sy as i32 + DY[r];
+            if tx < 0 || ty < 0 || tx >= N as i32 || ty >= N as i32 {
+                continue;
+            }
+            let mut v_puzzle = u_puzzle.clone();
+            v_puzzle
+                .f
+                .swap(u_puzzle.space, tx as usize * N + ty as usize);
+            v_puzzle.space = tx as usize * N + ty as usize;
+            let v_puzzle_key: String = puzzle_key(v_puzzle.clone());
+
+            if !v.contains(&v_puzzle_key) {
+                v.insert(v_puzzle_key);
+                v_puzzle.path = v_puzzle.path + DIR[r];
+                q.push_back(v_puzzle);
+            }
+        }
+    }
+    return "".to_string();
+}
+
+fn main() {
+    let mut init_numbers: Vec<usize> = vec![0; N2];
+    let mut init_space: usize = 0;
+    for i in 0..N2 {
+        let number: usize = read();
+        init_numbers[i] = number;
+
+        if init_numbers[i] == 0 {
+            init_numbers[i] = N2;
+            init_space = i;
+        }
+    }
+
+    let puzzle = Puzzle {
+        f: init_numbers,
+        space: init_space,
+        path: "".to_string(),
+    };
+
+    let ans: String = bfs(puzzle);
+    println!("{}", ans.len());
+}
