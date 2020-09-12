19346cc11ef1032f53142da10e36bec355e19e2c
regonn <programmer@haizine.art>
2020-09-12 11:21:26 +0900

    ALDS1_15_C

--- src/main.rs
+++ src/main.rs
@@ -14,31 +14,26 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
-    let n: u32 = read();
-    let mut w: f64 = read();
-
-    let mut vw: Vec<Vec<f64>> = vec![vec![0.0; 2]; n as usize];
+    let n: usize = read();
+    let mut st: Vec<(u32, u32)> = vec![(0, 0); n];
+    let mut time = 0;
+    let mut answer = 0;
 
     for i in 0..n {
-        let current_v: f64 = read();
-        let current_w: f64 = read();
-        vw[i as usize][0] = current_v;
-        vw[i as usize][1] = current_w;
+        let s: u32 = read();
+        let t: u32 = read();
+        st[i] = (s, t)
     }
 
-    vw.sort_by(|a, b| (b[0] / b[1]).partial_cmp(&(a[0] / a[1])).unwrap());
+    st.sort_by_key(|key| (key.1, key.0));
 
-    let mut ans: f64 = 0.0;
-
-    for i in 0..(n as usize) {
-        if w >= vw[i][1] {
-            w -= vw[i][1];
-            ans += vw[i][0];
-        } else {
-            ans += vw[i][0] / vw[i][1] * w;
-            w = 0.0;
+    for &(s, t) in &st {
+        if s < time {
+            continue;
         }
+        time = t + 1;
+        answer += 1;
     }
 
-    println!("{}", ans);
+    println!("{}", answer);
 }
